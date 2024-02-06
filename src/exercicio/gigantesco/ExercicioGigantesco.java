7f934b80430d7751c8873729c6abab41469f2321
962670 <962670@PITLAB2178806.senac.educ>
2024-02-06 08:15:49 -0300

    resposta

---
+++ src/exercicio/gigantesco/ExercicioGigantesco.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio.gigantesco;
+
+/**
+ *
+ * @author 962670
+ */
+public class ExercicioGigantesco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //exercicio 1:
+        System.out.println("Hello World");
+        
+        //exercicio 2:
+        //This is a single-line comment
+        /*This is a multi-line comment*/
+        
+        //exercicio 3:
+        String carName = "Volvo";
+        
+        //exercicio 4:
+        int maxSpeed = 120;
+        
+        //exercicio 5;
+        int x = 5;
+        int y = 10;
+        System.out.println(x + y);
+        
+        //exercicio 6:
+        int a = 5;
+        int b = 10;
+        int z = a + b;
+        System.out.println(z);
+        
+        //exercicio 7:
+        int c = 5, d = 6, f = 50;
+        System.out.println(c + d + f);
+        
+        //exercicio 8:
+        int myNum = 9;
+        float myFloatNum = 8.99f;
+        String myLetter = 'A';
+        
+        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
+                https://github.com/EduardoCardoso14/exerciciosJava.git
+        
+        
+    }
+    
+}
