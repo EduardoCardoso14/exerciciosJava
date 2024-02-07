26782ec0e8b4c897aa9f968b80c02b483d04bb07
962670 <962670@PITLAB2178806.senac.educ>
2024-02-07 08:01:43 -0300

    ate 23

--- src/exercicio/gigantesco/ExercicioGigantesco.java
+++ src/exercicio/gigantesco/ExercicioGigantesco.java
@@ -15,82 +15,114 @@ public class ExercicioGigantesco {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //exercicio 1:
         System.out.println("Hello World");
-        
+        System.out.println("----------");
+
         //exercicio 2:
         //This is a single-line comment
         /*This is a multi-line comment*/
-        
         //exercicio 3:
         String carName = "Volvo";
-        
+
         //exercicio 4:
         int maxSpeed = 120;
-        
+
         //exercicio 5;
         int x = 5;
         int y = 10;
         System.out.println(x + y);
-        
+        System.out.println("----------");
+
         //exercicio 6:
         int a = 5;
         int b = 10;
         int z = a + b;
         System.out.println(z);
-        
+        System.out.println("----------");
+
         //exercicio 7:
         int c = 5, d = 6, f = 50;
         System.out.println(c + d + f);
-        
+        System.out.println("----------");
+
         //exercicio 8:
         int myNum = 9;
         float myFloatNum = 8.99f;
         char myLetter = 'A';
         boolean MyBool = false;
         String MyText = "Hello World";
-        
+
         //exercicio 9:
-           //primitive data types;
-           
+        //primitive data types;
         //exercicio 10:
         double MyDouble = 9.78d;
-        int myInt = (int)MyDouble;
-        
+        int myInt = (int) MyDouble;
+
         //exercicio 11:
-        System.out.println(10*5);
-        
+        System.out.println(10 * 5);
+        System.out.println("----------");
+
         //exercicio 12:
-        System.out.println(10/5);
-        
+        System.out.println(10 / 5);
+        System.out.println("----------");
+
         //exercicio 13:
         int m = 10;
         ++m;
-        
+
         //exercicio 14:
         int n = 10;
         n = n + 5;
-        
+
         //exercicio 15:
         String greeting = "Hello";
-        
+
         //exercicio 16:
         String txt = "Hello";
         System.out.println(txt.toUpperCase());
-        
+        System.out.println("----------");
+
         //exercicio 17:
         String firstName = "John ";
         String lastName = "Doe";
-        System.out.println(firstName+lastName);
-        
+        System.out.println(firstName + lastName);
+        System.out.println("----------");
+
         //exercicio 18:
         String firstName2 = "John ";
         String lastName2 = "Doe";
         System.out.println(firstName.concat(lastName2));
+        System.out.println("----------");
+
+        //exercicio 19:
+        String hj = "Hello Everybody";
+        System.out.println(hj.indexOf('e'));
+        System.out.println("----------");
+
+        //exercicio 20:
+        int k = 5;
+        int l = 10;
+        Math.max(k, l);
+
+        //exercicio 21:
+        int raiz = 16;
+        Math.sqrt(raiz);
+
+        //exercicio 22:
+        Math.random();
+
+        //exercicio 23:
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+        System.out.println("----------");
         
-        //tamo com git repositorio novinho
+        //exercicio 23:
         
+
     }
-    
+
 }
